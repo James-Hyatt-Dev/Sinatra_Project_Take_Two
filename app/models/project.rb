1d56874badb5915c0c823bd066d4240fef20e9c8
prodigysurge <boatinglife6@yahoo.com>
2021-03-05 20:32:03 -0500

    adds validations to models

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,9 @@
 class Project < ActiveRecord::Base
+  
     belongs_to :user
+
+    validates :title, presence: true
+    validates :content, presence: true
+
   end
   
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,12 @@ class User < ActiveRecord::Base
   
   
     has_secure_password
+
+    validates :name, presence: true
+    validates :username, presence: true
+    validates :email, presence: true
+    validates :password, presence: true
+  
   
     def slug
       username.downcase.gsub(" ","-")
